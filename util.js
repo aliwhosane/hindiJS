6a8d15722d51c6f67ac3edd99aebbf07531d8ff1
Ali Husen <a.husen21@gmail.com>
2023-02-09 16:23:38 +0530

    utility class added

---
+++ util.js
@@ -0,0 +1,98 @@
+function isOperator(s) {
+  const operatorList = [
+    "=",
+    "+",
+    "-",
+    "*",
+    "/",
+    ">",
+    "<",
+    ">=",
+    "<=",
+    "==",
+    "!=",
+  ];
+  return operatorList.includes(s);
+}
+
+function isKeyword(s) {
+  const keywordList = [
+    "abstract",
+    "break",
+    "char",
+    "debugger",
+    "double",
+    "export",
+    "finally",
+    "goto",
+    "in",
+    "let",
+    "null",
+    "public",
+    "super",
+    "throw",
+    "try",
+    "volatile",
+    "arguments",
+    "byte",
+    "class",
+    "default",
+    "else",
+    "extends",
+    "float",
+    "if",
+    "instanceof",
+    "long",
+    "package",
+    "return",
+    "switch",
+    "throws",
+    "typeof",
+    "while",
+    "await",
+    "case",
+    "const",
+    "delete",
+    "enum",
+    "FALSE",
+    "for",
+    "implements",
+    "int",
+    "native",
+    "private",
+    "short",
+    "synchronized",
+    "transient",
+    "var",
+    "with",
+    "boolean",
+    "catch",
+    "continue",
+    "do",
+    "eval",
+    "final",
+    "function",
+    "import",
+    "interface",
+    "new",
+    "protected",
+    "static",
+    "this",
+    "TRUE",
+    "void",
+    "yield",
+  ];
+  return keywordList.includes(s);
+}
+
+function isAlphabet(s) {
+  return (str > "a" && str <= "z") || (str >= "A" && str <= "Z") || str == "_";
+}
+
+function isNumber(s) {
+  return !isNaN(parseFloat(s)) && isFinite(s);
+}
+
+function isAlphaNumeric(s) {
+  return isAlphabet(s) || isNumber(s);
+}
