60431e812ff65e068ac0eee177ea2ba0f3577e9b
Ali Husen <a.husen21@gmail.com>
2023-03-15 15:41:27 +0530

    parser completed

--- parser/parser.js
+++ parser/parser.js
@@ -50,5 +50,122 @@ export class Parser {
     this.value = null;
     this.start = this.end = this.pos;
     this.startLoc = this.endLoc = this.curPosition();
+
+    this.lastTokEndLoc = this.lastTokStartLoc = null;
+    this.lastTokStart = this.lastTokEnd = this.pos;
+
+    this.context = this.initialContext();
+    this.exprAllowed = true;
+
+    this.inModule = options.sourceType === "module";
+    this.strict = this.inModule || this.strictDirective(this.pos);
+
+    this.potentialArrowAt = -1;
+    this.potentialArrowInForAwait = false;
+
+    this.yieldpos = this.awaitPos = this.awaitIdentPos = 0;
+    this.labels = [];
+    this.undefinedExports = Object.create(null);
+
+    if (
+      this.pos === 0 &&
+      options.allowHashBang &&
+      this.input.slice(0, 2) == "#!"
+    )
+      this.skipLineComment(2);
+
+    this.scopeStack = [];
+    this.enterScope(SCOPE_TOP);
+
+    this.regexpState = null;
+    this.privateNameStack = [];
+  }
+
+  parse() {
+    let node = this.options.program || this.startNode();
+    this.nextToken();
+    return this.patseTopLevel(node);
+  }
+
+  get inFunction() {
+    return (this.currentVarScope().flags & SCOPE_FUNCTION) > 0;
+  }
+
+  get inGenerator() {
+    return (
+      (this.currentVarScope().flags & SCOPE_GENERATOR) > 0 &&
+      !this.currentVarScope().inClassFieldInit
+    );
+  }
+
+  get inAsync() {
+    return (
+      (this.currentVarScope().flags & SCOPE_ASYNC) > 0 &&
+      !this.currentVarScope().inClassFieldInit
+    );
+  }
+
+  get canAwait() {
+    for (let i = this.scopeStack.length - 1; i >= 0; i--) {
+      let scope = this.scopeStack[i];
+      if (scope.inClassFieldInit || (scope.flags && SCOPE_CLASS_STATIC_BLOCK))
+        return false;
+      if (scope.flags & SCOPE_FUNCTION) return (scope.flags & SCOPE_ASYNC) > 0;
+    }
+
+    return (
+      (this.inModule && this.options.ecmaVersion >= 13) ||
+      this.options.allowAwaitOutsideFunction
+    );
+  }
+  get allowSuper() {
+    const { flags, inClassFieldInit } = this.currentThisScope();
+    return (
+      (flags & SCOPE_SUPER) > 0 ||
+      inClassFieldInit ||
+      this.options.allowSuperOutsideMethod
+    );
+  }
+
+  get allowDirectSuper() {
+    return (this.currentThisScope().flags & SCOPE_DIRECT_SUPER) > 0;
+  }
+
+  get treatFunctionAsVar() {
+    return this.treatFunctionAsVarInScope(this.currentScope());
+  }
+
+  get allowNewDotTarget() {
+    const { flags, inClassFieldInit } = this.currentThisScope();
+    return (
+      (flags & (SCOPE_FUNCTION | SCOPE_CLASS_STATIC_BLOCK)) > 0 ||
+      inClassFieldInit
+    );
+  }
+
+  get inClassStaticBlock() {
+    return (this.currentVarScope().flags & SCOPE_CLASS_STATIC_BLOCK) > 0;
+  }
+
+  static extend(...plugins) {
+    let cls = this;
+    for (let i = 0; i < plugins.length; i++) {
+      cls = plugins[i](cls);
+      return cls;
+    }
+  }
+
+  static parse(input, options) {
+    return new this(options, input).parse();
+  }
+
+  static parseExpressionAt(input, pos, options) {
+    let parser = new this(options, input, pos);
+    this.parser.nextToken();
+    return parser.parseExpression();
+  }
+
+  static tokenizer(input, options) {
+    return new this(options, input);
   }
 }
